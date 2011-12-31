8cf2d50a0a6794a1ff889a3927286ac2b7b8623d
Craig Condon <craig@spiceapps.com>
2011-12-31 00:29:30 -0600

    refactored

--- test/index.js
+++ test/index.js
@@ -14,14 +14,19 @@ vows.describe('Sifter').addBatch({
 
 
 			'has a sifted $in count of 3': function(topic) {
-
 				assert.isTrue(sift({$in:['craig','john','joe']}, topic).length == 3);
 
 			},
 
 			'has a sifted $nin count of 1': function(topic) {
-				
 				assert.isTrue(sift({$nin:['craig','john','joe']}, topic).length == 1);
+			},
+
+			/**
+			 */
+
+			'has a sifted $all count of 1': function(topic) {
+				assert.isTrue(sift({$all:['craig']}, topic).length == 1);
 			}
 		}
 	}

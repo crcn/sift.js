6c86f48d6d419610137e6876f592c1e2b95608ec
Craig Condon <craig@spiceapps.com>
2014-11-06 21:59:55 -0800

    add regex test

---
+++ test/$regex-test.js
@@ -0,0 +1,14 @@
+var _ = require('underscore'),
+    expect = require("expect.js"),
+    sift = require(".."),
+    assert = require("assert");
+
+
+describe("$regex#", function () {
+    xit("can be evaluated with an object", function () {
+
+    });
+    xit("can be evaluated with a string", function () {
+
+    });
+});

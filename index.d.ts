44dda91c5afcd02a63aaabf884e57e8018f35cae
Craig Condon <craig.j.condon@gmail.com>
2016-07-21 11:22:41 +0200

    update typings

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module "sift" {
-  function sift(query:Object):Function;
+  function sift(query:Object):(value:any, index:number, array:any[]) => boolean;
   namespace sift {
   }
   export = sift;

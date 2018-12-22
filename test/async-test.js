9bba39847c3327abdda7e7e5fa145933d286e365
Craig Condon <craig.j.condon@gmail.com>
2018-12-22 09:51:41 -0600

    start on async tests

---
+++ test/async-test.js
@@ -0,0 +1,63 @@
+import * as assert from "assert";
+import sift from "..";
+
+describe(__filename + "#", () => {
+  [
+    [
+      "can use a simple async $eq filter",
+      {
+        $eq: function(value) {
+          return new Promise(function(resolve) {
+            resolve(value > 2);
+          });
+        }
+      },
+      [1, 2, 3, 4, 5],
+      [3, 4, 5]
+    ]
+
+    // [
+    //   "can use a simple async $or filter",
+    //   {
+    //     $and: [
+    //       function(value) {
+    //         new Promise(function(resolve) {
+    //           resolve(value > 2);
+    //         })
+    //       },
+    //       function(value) {
+    //         new Promise(function(resolve) {
+    //           resolve(value < 5);
+    //         })
+    //       }
+    //     ]
+    // }, [1, 2, 3, 4, 5], [3, 4]]
+  ].forEach(function([description, query, values, result]) {
+    it(description, function() {
+      return new Promise(function(resolve, reject) {
+        var filter = asyncFilter(sift(query));
+        filter(values).then(function(filteredValues) {
+          try {
+            assert.equal(
+              JSON.stringify(filteredValues),
+              JSON.stringify(result)
+            );
+            resolve();
+          } catch (e) {
+            reject(e);
+          }
+        });
+      });
+    });
+  });
+});
+
+function asyncFilter(filter) {
+  return function(values) {
+    return new Promise(function(resolve, reject) {
+      Promise.all(values.map(filter)).then(function(filtered) {
+        resolve(values.filter((value, index) => filtered[index]));
+      });
+    });
+  };
+}
